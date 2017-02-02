6560d584b282f14143dad60c8bb83ac20491c9bb
tiphurin <tiphurin@hotmail.com>
2017-02-02 20:59:56 +0700

    MyCalculator.java
    Version 3.1

--- MyCalculator.java
+++ MyCalculator.java
@@ -15,22 +15,19 @@ public class MyCalculator {
 		System.out.println("Enter 2 >> Minus    (-)");
 		System.out.println("Enter 3 >> Multi    (*)");
 		System.out.println("Enter 4 >> Divide   (/)");
-		System.out.println("Enter 5 >> SqureRoot");
-		System.out.println("Enter 6 >> Power    (^)");
-		System.out.println("Enter 7 >> Mean     ");
+		System.out.println("Enter 5 >> Mean     ");
 		
 		System.out.print("Please choose function : ");
 		int operator = input.nextInt();
 		
-		if(operator==1 || operator==2 || operator==3 || operator==4
-				|| operator==6){
+		if(operator==1 || operator==2 || operator==3 || operator==4){
 			System.out.print("Enter num1 :");
 			num1 = input.nextDouble();
 		
 			System.out.print("Enter num2 :");
 			num2 = input.nextDouble();
 		}
-		else if(operator == 7){
+		else {
 			System.out.print("Enter amount of number :");
 			num1 = input.nextDouble();
 			
@@ -40,10 +37,7 @@ public class MyCalculator {
 				num2 = num2+input.nextDouble();
 			}
 		}
-		else{
-			System.out.print("Enter num1 :");
-			num1 = input.nextDouble();
-		}
+		
 		
 		
 		if(operator == 1){
@@ -63,14 +57,6 @@ public class MyCalculator {
 			System.out.println(num1+" / "+num2+" = "+sum);
 		}
 		else if(operator == 5){
-			double sum = Math.sqrt(num1);
-			System.out.println("Squre Root of "+num1+" = "+sum);
-		}
-		else if(operator == 6){
-			double sum = Math.pow(num1, num2);
-			System.out.println(num1+" ^ "+num2+" = "+sum);
-		}
-		else if(operator == 7){
 			double sum = num2/num1;
 			System.out.println("Mean is "+sum);
 		}
